0070103eb70cd56664d495897c3e323fa5da1761
Samuel Catalano <samuelcatalano@Samuels-MBP.home>
2021-06-29 12:55:21 +0100

    Push files to GitHub

---
+++ app/src/main/java/notonthehighstreet/model/base/BaseModel.java
@@ -0,0 +1,9 @@
+package notonthehighstreet.model.base;
+
+import java.io.Serializable;
+
+public class BaseModel implements Serializable {
+
+    // in the future if I want to define a common behavior for all models
+    // I can construct the logic in the class
+}
